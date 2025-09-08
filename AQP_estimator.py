update non-equi join calculation fix labeling overflow

AQP_estimator.py
@@ -8,14 +8,19 @@ from dataclasses import dataclass
 import common
 from datasets import JoinOrderBenchmark
 
+from model.made import MADE
+
 from common import Table
-from model import made
+from model import made, transformer
+
 import pandas as pd
 from typing import *
 from enum import Enum
 from torch.cuda.amp import autocast
-from utils import  util
-
+from train import DataParallelPassthrough
+from utils import train_utils, util
+import utils
+import utils.util
 
 real_table = []
 OPS_array = np.array(['=', '>', '<', '>=', '<='])
@@ -486,6 +491,10 @@ class DirectEstimator(CardEst):
         self.inps = {name: torch.full((1, 2, len(table.columns)), -1, device=self.device) for name, table in self.fact_tables.items()}
         self.preds = {name: torch.zeros((1, 2, 5 * len(table.columns)), device=self.device) for name, table in self.fact_tables.items()}
         self._warm_up_model() #预热以防第一次预测开销过高导致速度测量不准确
+        if self.models is not None:
+            for m_name, m in self.models.items():
+                ANPM_param_num = np.array([np.prod(t.shape) for t in [v for k, v in m.named_parameters() if 'modulation_offset' in k]]).sum()
+                print(f'{m_name} ANPM params num: {ANPM_param_num}')
 
     def __str__(self):
         return 'Duet_Join'
@@ -515,9 +524,15 @@ class DirectEstimator(CardEst):
         with autocast():
             prob = self.get_prob_of_predicates_from_different_table(predicate_list, join_tables, tables_dict, how,
                                                                     real=real)
-            # if not self.config['test']['faster_version']:
-            #     prob = torch.clip(prob, 0.0, 1.0)
-            return prob.sum().cpu().item()
+            if self.config['test']['faster_version']:
+                if isinstance(prob, torch.Tensor):
+                    return sum(prob.cpu().numpy().tolist())
+                elif isinstance(prob, np.ndarray):
+                    return sum(prob.tolist())
+                else:
+                    return prob
+            else:
+                return prob.sum().cpu().item()
 
 
 
@@ -540,6 +555,9 @@ class DirectEstimator(CardEst):
             dist = torch.zeros((len(self.global_distinct_values)), device=local_distribution.device,
                                dtype=local_distribution.dtype)
             dist[mask] = local_distribution + 1e-10 if not real and how=='=' else local_distribution
+            if self.config['test']['faster_version']:
+                dist = dist.cpu().numpy().astype(object)
+                # dist = np.array([gmpy2.mpz(d) for d in dist])
             return dist
         else:
             raise Exception(
@@ -559,8 +577,8 @@ class DirectEstimator(CardEst):
 
         # 首先将所有涉及到的表分为两份
         join_tables = list(join_tables)
-        tables_left = join_tables[:len(join_tables) // 2]
-        tables_right = join_tables[len(join_tables) // 2:]
+        tables_left = join_tables[:-1]
+        tables_right = join_tables[-1:]
         # 分离来自左右两组表的谓词
         predicates_from_left = list(filter(lambda x: x.table in tables_left, predicates))
         predicates_from_right = list(filter(lambda x: x.table in tables_right, predicates))
@@ -581,11 +599,6 @@ class DirectEstimator(CardEst):
                     prob_left = self.get_freq_of_predicates(tuple(predicates_from_left))
                 else:
                     prob_left = self.get_prob_of_predicates(predicates_from_left, keepdim=self.get_keep_dim(predicates_from_left))
-                # prob_pred = self.get_freq_of_predicates(predicates_from_left, tables_dict, is_single_table=True)
-                # print(f'table: {tables_left[0]}, true sel: {prob_pred.sum()}, predicted sel: {prob_left.sum()}')
-                # prob_left = self.get_prob_of_predicates(predicates_from_left, distinct_table,
-                #                                             keepdim=self.get_keep_dim(predicates_from_left))
-                # prob_left = prob_pred * prob_left
                 prob_left = self.fill_complete_values(prob_left, left_distinct_values, tables_left[0], how, real)
             elif len(tables_left) > 1:
                 prob_left = self.get_prob_of_predicates_from_different_table(predicates_from_left, tables_left,
@@ -593,14 +606,6 @@ class DirectEstimator(CardEst):
                                                                              how, reduce=False, real=real)
             else:
                 prob_left = 1
-
-            if len(tables_right) > 0 and len(tables_left)>0: # 避免单表基数估计时的错误
-                if how == '<=':
-                    prob_left = torch.cumsum(prob_left, dim=-1)
-                elif how == '<':
-                    prob_left = torch.roll(prob_left, shifts=1, dims=-1)
-                    prob_left[:1] = 0
-                    prob_left = torch.cumsum(prob_left, dim=-1)
 
             if len(tables_right) == 1:
                 # 调整顺序始终把key列放最后
@@ -611,11 +616,6 @@ class DirectEstimator(CardEst):
                     prob_right = self.get_freq_of_predicates(tuple(predicates_from_right))
                 else:
                     prob_right = self.get_prob_of_predicates(predicates_from_right, keepdim=self.get_keep_dim(predicates_from_right))
-                # prob_pred = self.get_freq_of_predicates(predicates_from_right, tables_dict, is_single_table=True)
-                # print(f'table: {tables_right[0]}, true sel: {prob_pred.sum()}, predicted sel: {prob_right.sum()}')
-                # prob_right = self.get_prob_of_predicates(predicates_from_right, distinct_table,
-                #                                              keepdim=self.get_keep_dim(predicates_from_right))
-                # prob_right = prob_pred * prob_right
                 prob_right = self.fill_complete_values(prob_right, right_distinct_values, tables_right[0], how, real)
             elif len(tables_right)>1:
                 prob_right = self.get_prob_of_predicates_from_different_table(predicates_from_right, tables_right,
@@ -625,12 +625,32 @@ class DirectEstimator(CardEst):
                 prob_right = 1
 
             if len(tables_left) > 0 and len(tables_right)>0: # 避免单表基数估计时的错误
-                if how == '>=':
-                    prob_right = torch.cumsum(prob_right, dim=-1)
-                elif how == '>':
-                    prob_right = torch.roll(prob_right, shifts=1, dims=-1)
-                    prob_right[:1] = 0
-                    prob_right = torch.cumsum(prob_right, dim=-1)
+                if isinstance(prob_left, torch.Tensor):
+                    if how == '>=':
+                        prob_left = prob_left + torch.sum(prob_left, dim=-1, keepdim=True) - torch.cumsum(prob_left, dim=-1)
+                    elif how == '>':
+                        prob_left = torch.roll(prob_left, shifts=-1, dims=-1)  # 反向移位
+                        prob_left[-1:] = 0
+                        prob_left = prob_left + torch.sum(prob_left, dim=-1, keepdim=True) - torch.cumsum(prob_left, dim=-1)
+                    elif how == '<=':
+                        prob_left = torch.cumsum(prob_left, dim=-1)
+                    elif how == '<':
+                        prob_left = torch.roll(prob_left, shifts=1, dims=-1)
+                        prob_left[:1] = 0
+                        prob_left = torch.cumsum(prob_left, dim=-1)
+                elif isinstance(prob_left, np.ndarray):
+                    if how == '>=':
+                        prob_left = prob_left + np.sum(prob_left, axis=-1, keepdims=True) - np.cumsum(prob_left, axis=-1)
+                    elif how == '>':
+                        prob_left = np.roll(prob_left, shift=-1, axis=-1)  # 反向移位
+                        prob_left[-1:] = 0
+                        prob_left = prob_left + np.sum(prob_left, axis=-1, keepdims=True) - np.cumsum(prob_left, axis=-1)
+                    elif how == '<=':
+                        prob_left = np.cumsum(prob_left, axis=-1)
+                    elif how == '<':
+                        prob_left = np.roll(prob_left, shift=1, axis=-1)
+                        prob_left[:1] = 0
+                        prob_left = np.cumsum(prob_left, axis=-1)
 
         if not self.config['test']['faster_version']:
             if len(tables_left) == 1:
@@ -645,14 +665,6 @@ class DirectEstimator(CardEst):
                                                                                  how, real=real, key_only=True)
             else:
                 prob_key_left = 1
-
-            if len(tables_right) > 0 and len(tables_left)>0:
-                if how == '<=':
-                    prob_key_left = torch.cumsum(prob_key_left, dim=-1)
-                elif how == '<':
-                    prob_key_left = torch.roll(prob_key_left, shifts=1, dims=-1)
-                    prob_key_left[:1] = 0
-                    prob_key_left = torch.cumsum(prob_key_left, dim=-1)
 
             if len(tables_right) == 1:
                 if real or tables_right[0] in real_table:
@@ -669,12 +681,32 @@ class DirectEstimator(CardEst):
                 prob_key_right = 1
 
             if len(tables_left) > 0 and len(tables_right) > 0:
-                if how == '>=':
-                    prob_key_right = torch.cumsum(prob_key_right, dim=-1)
-                elif how == '>':
-                    prob_key_right = torch.roll(prob_key_right, shifts=1, dims=-1)
-                    prob_key_right[:1] = 0
-                    prob_key_right = torch.cumsum(prob_key_right, dim=-1)
+                if isinstance(prob_key_left, torch.Tensor):
+                    if how == '>=':
+                        prob_key_left = prob_key_left + torch.sum(prob_key_left, dim=-1, keepdim=True) - torch.cumsum(prob_key_left, dim=-1)
+                    elif how == '>':
+                        prob_key_left = torch.roll(prob_key_left, shifts=-1, dims=-1)  # 反向移位
+                        prob_key_left[-1:] = 0
+                        prob_key_left = prob_key_left + torch.sum(prob_key_left, dim=-1, keepdim=True) - torch.cumsum(prob_key_left, dim=-1)
+                    elif how == '<=':
+                        prob_key_left = torch.cumsum(prob_key_left, dim=-1)
+                    elif how == '<':
+                        prob_key_left = torch.roll(prob_key_left, shifts=1, dims=-1)
+                        prob_key_left[:1] = 0
+                        prob_key_left = torch.cumsum(prob_key_left, dim=-1)
+                elif isinstance(prob_key_left, np.ndarray):
+                    if how == '>=':
+                        prob_key_left = prob_key_left + np.sum(prob_key_left, axis=-1, keepdims=True) - np.cumsum(prob_key_left, axis=-1)
+                    elif how == '>':
+                        prob_key_left = np.roll(prob_key_left, shift=-1, axis=-1)  # 反向移位
+                        prob_key_left[-1:] = 0
+                        prob_key_left = prob_key_left + np.sum(prob_key_left, axis=-1, keepdims=True) - np.cumsum(prob_key_left, axis=-1)
+                    elif how == '<=':
+                        prob_key_left = np.cumsum(prob_key_left, axis=-1)
+                    elif how == '<':
+                        prob_key_left = np.roll(prob_key_left, shift=1, axis=-1)
+                        prob_key_left[:1] = 0
+                        prob_key_left = np.cumsum(prob_key_left, axis=-1)
 
         if not key_only:
             # 计算两个合取式中包含两个表的公式
@@ -682,11 +714,10 @@ class DirectEstimator(CardEst):
                 prob = prob_left * prob_right
             else:
                 prob = (prob_left * prob_right) / torch.sum(prob_key_left * prob_key_right)
-            # 对key的维度求和，前面带着key维度计算，等于并行地对key的每一个可能取值进行计算
-            # if reduce:
-            #     prob = prob.sum(dim=-1)
-            #     if len(prob.shape) == 0:
-            #         prob = prob.view(1, )
+            # assert (prob >= 0).all() , f'prob error: {prob}'
+            # assert not torch.isnan(prob).all() if isinstance(prob, torch.Tensor) else True, f'prob error: {prob}'
+            # assert not torch.isinf(prob).all() if isinstance(prob, torch.Tensor) else True, f'prob error: {prob}'
+            # assert prob.sum() >=0, f'prob error: {prob}'
             return prob
         else:
             assert not self.config['test']['faster_version']
@@ -724,7 +755,7 @@ class DirectEstimator(CardEst):
                             raise Exception(f'not supported predicate: {predicate.raw_attr}{true_predicate}{true_val}')
             else:
                 pred_key.append(predicate)
-        filtered_df = df[mask] if len(predicates)>1 else df
+        filtered_df = self.tables_dict[predicates[0].table].data[mask]
         if len(pred_key) == 0:
             freq = torch.as_tensor([len(filtered_df)], device=util.get_device())
         elif len(pred_key) == 1:
@@ -733,7 +764,7 @@ class DirectEstimator(CardEst):
 
             # 使用 torch.unique 获取频次
             unique, counts = torch.unique(key_values, return_counts=True)
-            counts = counts.to(torch.float)
+            counts = counts.to(torch.float64)
             if key_col.has_none:
                 isnan_mask = torch.isnan(unique)
                 # 处理 NaN 逻辑
@@ -743,7 +774,7 @@ class DirectEstimator(CardEst):
 
             # 构建最终频次数组
             all_values = key_col.all_distinct_values_gpu
-            freq = torch.zeros(len(all_values), device=util.get_device())
+            freq = torch.zeros(len(all_values), device=util.get_device(), dtype=torch.float64)
             isin_mask = torch.isin(all_values, unique)
             freq[isin_mask] = counts
             if key_col.has_none:
@@ -753,7 +784,9 @@ class DirectEstimator(CardEst):
             raise Exception('more than one keep dim column, not support yet')
         if not self.config['test']['faster_version']:
             freq = freq / len(df)
+            return freq
         # self.cache_true[frozenset([frozenset(predicates)])] = freq.cpu()
+        freq = freq.to(torch.int64)
         return freq
 
 

GetTableAndWorkloadSummary.py
@@ -93,7 +93,7 @@ for table_name, key in JoinOrderBenchmark.GetJobLightJoinKeys().items():
 all_true_cards = {}
 all_base_cards = {}
 for workload in workloads:
-    queries_job_format = utils.util.JobToQuery(f'./queries/{workload}.csv')
+    queries_job_format, _ = utils.util.JobToQuery(f'./queries/{workload}.csv')
     loaded_queries = utils.util.UnpackQueries(tables_dict, queries_job_format)
     preds = []
     for query in loaded_queries:
@@ -112,7 +112,7 @@ for workload in workloads:
             true_card_list = np.array(pkl.load(f))
         base_cards = []
         for query_id, (join_tables, join_keys, preds, true_card) in enumerate(loaded_queries):
-            base_cards.append(JoinOrderBenchmark.TRUE_JOIN_BASE_CARDINALITY[how][frozenset(join_tables)])
+            base_cards.append(JoinOrderBenchmark.TRUE_JOIN_BASE_CARDINALITY[how][str(join_tables)])
         base_cards = np.array(base_cards)
         print(f'workload: {workload_tag}, how: {how}, min_card:{np.format_float_scientific(true_card_list.min())}, max_card:{np.format_float_scientific(true_card_list.max())}, min_base_card:{np.format_float_scientific(base_cards.min())}, max_base_card:{np.format_float_scientific(base_cards.max())}')
         all_true_cards[workload_tag+how] = true_card_list

collect_result.py
@@ -8,12 +8,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 import AQP_estimator
+from utils.util import get_q_error
 
 train_tags = {'DistJoin': ['', '_previous'],
         'PG':['', '_previous'],
         'NeuroCard':['', '_previous'],
         'FactorJoin':['', '_previous'],
-        'MSCN':['', '_previous']}
+        'MSCN':['', '_previous'],
+        'ALECE':['', '_previous'],
+        'UAE': ['', '_previous'],
+        }
 eval_tags = {
     '1740979448.161846': [''],
     # '1740289713.764569': [''],
@@ -22,10 +26,13 @@ eval_tags = {
     'PG': ['', '_previous'],
     'result': ['', '_previous'],
     'FactorJoin': ['', '_previous'],
-    'MSCN':['', '_previous']
+    'MSCN':['', '_previous'],
+    'ALECE': ['', '_previous'],
+    'UAE': ['', '_previous'],
+
 }
 hows = AQP_estimator.OPS_array.tolist()
-methods = ['DistJoin','PG', 'NeuroCard', 'FactorJoin', 'MSCN'] # must load DistJoin first to get selectivity column
+methods = ['DistJoin', 'PG', 'NeuroCard', 'FactorJoin', 'MSCN', 'ALECE', 'UAE'] #x must load DistJoin first to get selectivity column
 DistJoin_tag_to_exp_marks = {
     'job-light': '1740979448.161846',
     'job-light-ranges': '1740979448.161846',
@@ -46,6 +53,8 @@ PG_result_path = './result/PG'
 NeuroCard_path = './result/NeuroCard'
 FactorJoin_path = './result/FactorJoin'
 MSCN_path = './result/MSCN'
+ALECE_path = './result/ALECE'
+UAE_path = './result/UAE'
 result_dict = {'method': [], 'train_tag':[], 'eval_tag':[], 'how':[], 'workload':[], 'q_error':[], 'rel_error':[], 'cost':[], 'selectivity':[], 'est_card':[], 'true_card':[]}
 
 def get_exp_mark(method):
@@ -53,7 +62,7 @@ def get_exp_mark(method):
         exp_mark = DistJoin_tag_to_exp_marks[workload + train_tag]
     elif method == 'PG':
         exp_mark = 'PG'
-    elif method == 'NeuroCard':
+    elif method in ['NeuroCard', 'UAE']:
         exp_mark = 'result'
     else:
         exp_mark = method
@@ -69,8 +78,10 @@ for method in methods:
                 if method == 'PG' and train_tag!=eval_tag:
                     continue
                 for how in hows:
+                    distjoin_mark = get_exp_mark('DistJoin')
+                    distjoin_df = pd.read_csv(os.path.join(DistJoin_path, distjoin_mark, f'{distjoin_mark}_{workload}_{how}{eval_tag}.csv'))
                     if method == 'DistJoin':
-                        df = pd.read_csv(os.path.join(DistJoin_path, exp_mark, f'{exp_mark}_{workload}_{how}{eval_tag}.csv'))
+                        df = distjoin_df
                     elif method == 'PG':
                         df = pd.read_csv(os.path.join(PG_result_path, f'{workload}_{how}{eval_tag}.csv'))
                     elif method == 'NeuroCard':
@@ -83,6 +94,20 @@ for method in methods:
                         df = pd.read_csv(os.path.join(FactorJoin_path, f'{workload_map_for_FactorJoin[workload]}{train_tag}{eval_tag}.csv'))
                     elif method == 'MSCN':
                         df = pd.read_csv(os.path.join(MSCN_path, f'{workload}-{workload_to_num[workload]}queries-seed42-{how}{train_tag}{eval_tag}.csv'))
+                    elif method == 'ALECE':
+                        df = pd.read_csv(os.path.join(ALECE_path, f'{workload}-{how}{eval_tag}.csv'))
+                    elif method == 'UAE':
+                        if how != '=':
+                            continue
+                        df = pd.read_csv(os.path.join(UAE_path, f'{exp_mark}_[\'{workload}-mscn-workload{train_tag.replace('_', '-')}{eval_tag.replace('_', '-')}\'].csv'))
+                    else:
+                        raise Exception(f'not supported method:{method}')
+                    if method in ['FactorJoin', 'ALECE']: # fix the outdated true_card and errors
+                        df['true_card'] = distjoin_df['true_card'] if len(df) == len(distjoin_df) else distjoin_df['true_card'][(distjoin_df['true_card']!=0) & (distjoin_df['true_card']!='0')]
+                        est_cards = df['est_card']
+                        true_cards = np.array([int(c) for c in list(df['true_card'])],dtype=object)
+                        df['q_error'] = np.array([get_q_error(int(est_card), int(true_card)) for est_card, true_card in zip(est_cards, true_cards)])
+                        df['rel_error'] = np.array([np.abs(est_card - true_card) / true_card if true_card!=0 else np.nan for est_card, true_card in zip(est_cards, true_cards)])
                     df = df[(df['true_card'] != 0) & (df['true_card'] != '0')]
                     print(f'{method} {workload}, {train_tag}, {eval_tag}, {how}')
                     if 'selectivity' in df.columns:
@@ -105,7 +130,7 @@ for method in methods:
                     result_dict['eval_tag'].extend([eval_tag for _ in range(df.shape[0])])
                     result_dict['how'].extend([how for _ in range(df.shape[0])])
                     result_dict['est_card'].extend(df['est_card'])
-                    result_dict['true_card'].extend(df['true_card'])
+                    result_dict['true_card'].extend([int(v) for v in df['true_card'].values])
 result_df = pd.DataFrame(result_dict)
 result_df.to_csv('result.csv', index=False, na_rep="")
 result_summary = {'method': [], 'train_tag':[], 'eval_tag':[], 'how':[], 'workload':[], 'q_error_mean':[],'q_error_50th':[],'q_error_95th':[],'q_error_99th':[],'q_error_100th':[], 'rel_error_mean':[], 'cost_mean':[]}
@@ -121,7 +146,7 @@ for method in methods:
                 if method == 'PG' and train_tag!=eval_tag:
                     continue
                 for how in hows:
-                    if method in ['NeuroCard', 'FactorJoin'] and how != '=':
+                    if method in ['NeuroCard', 'FactorJoin', 'UAE'] and how != '=':
                         continue
                     filted = result_df[(result_df['method'] == method) & (result_df['train_tag'] == train_tag) & (result_df['eval_tag'] == eval_tag) & (result_df['how'] == how) & (result_df['workload'] == workload)]
                     result_summary['method'].append(method)

eval-IMDB-all.py
@@ -152,7 +152,7 @@ def RunN(tables_dict,
 
     real = config['real']
     tc = []
-    queries_job_format = utils.util.JobToQuery(config['queries_csv'])
+    queries_job_format, _ = utils.util.JobToQuery(config['queries_csv'])
     queries_job_format_ = []
     for idx, (involved_tables, _, _, _) in enumerate(queries_job_format):
         if len(set(JoinOrderBenchmark.GetJobLightJoinKeys().keys()).intersection(set(involved_tables))) < len(
@@ -216,7 +216,7 @@ def RunN(tables_dict,
         st = time.time()
         condition_prob = estimator.get_prob_of_predicate_tree(predicates, join_tables, tables_dict, how, real=real)
         st = time.time() - st
-        base_card = datasets.JoinOrderBenchmark.TRUE_JOIN_BASE_CARDINALITY[how][frozenset(join_tables)]
+        base_card = datasets.JoinOrderBenchmark.TRUE_JOIN_BASE_CARDINALITY[how][str(join_tables)]
         pred_card = np.round(condition_prob) if config['faster_version'] else np.ceil(condition_prob*base_card)
         result_list.append(pred_card)
         # print(f'prob:{condition_prob}, pred_card:{pred_card}, true_card:{true_card}, time cost: {st} s')
@@ -280,7 +280,7 @@ def RunN(tables_dict,
     q_low_df = pd.DataFrame(q_error_low)
     print(
         f'\tMean: {q_low_df.mean().to_numpy().item()}, 50th: {q_low_df.quantile(q=0.5).to_numpy().item()}, 95th: {q_low_df.quantile(q=0.95).to_numpy().item()}, 99th: {q_low_df.quantile(q=0.99).to_numpy().item()}, max:{q_low_df.max().to_numpy().item()}')
-    q_df = pd.DataFrame(q_error_list)
+    q_df = pd.DataFrame(q_error_list, columns=['q_error'])
     prec_summary = {
         'mean q_error': np.round(q_df.mean().item(),3).item(),
         '50th q_error': np.round(q_df.quantile(q=0.5).to_numpy().item(),3).item(),
@@ -298,8 +298,8 @@ def RunN(tables_dict,
     csv_res = pd.DataFrame({'true_card': true_card_list, 'est_card': result_list, 'selectivity': selectivity_list, 'q_error': q_error_list, 'rel_error': rel_error_list, 'cost': tc})
     os.makedirs(f'./result/DistJoin/{exp_mark}/', exist_ok=True)
     csv_res.to_csv(f'./result/DistJoin/{exp_mark}/{exp_mark}_{workload_tag}_{how}{tag}.csv')
-    summary['max_q_error'] = q_df.max().to_numpy().item()
-    return q_df.max().to_numpy().item()
+    summary['max_q_error'] = q_df['q_error'].max()
+    return q_df['q_error'].max()
 
 
 def SaveEstimators(path, estimators, return_df=False):

eval-IMDB-epochs.py
@@ -152,7 +152,7 @@ def RunN(tables_dict,
     real = config['real']
 
     tc = []
-    queries_job_format = utils.util.JobToQuery(config['queries_csv'])
+    queries_job_format, _ = utils.util.JobToQuery(config['queries_csv'])
     queries_job_format_ = []
     for idx, (involved_tables, _, _, _) in enumerate(queries_job_format):
         if len(set(JoinOrderBenchmark.GetJobLightJoinKeys().keys()).intersection(set(involved_tables))) < len(
@@ -219,7 +219,7 @@ def RunN(tables_dict,
         st = time.time()
         condition_prob = estimator.get_prob_of_predicate_tree(predicates, join_tables, tables_dict, how, real=real)
         st = time.time() - st
-        base_card = datasets.JoinOrderBenchmark.TRUE_JOIN_BASE_CARDINALITY[how][frozenset(join_tables)]
+        base_card = datasets.JoinOrderBenchmark.TRUE_JOIN_BASE_CARDINALITY[how][str(join_tables)]
         pred_card = np.round(condition_prob) if config['faster_version'] else np.ceil(condition_prob*base_card)
         result_list.append(pred_card)
         print(f'prob:{condition_prob}, pred_card:{pred_card}, true_card:{true_card}, time cost: {st} s')

train.py
@@ -21,7 +21,7 @@ from torch.utils import data
 import datasets
 from model import transformer, made
 from datasets import JoinOrderBenchmark
-from model.TesseractTransformer import TesseractTransformer
+#from model.TesseractTransformer import TesseractTransformer
 from model.transformer import Transformer
 from utils import train_utils, util
 import utils

utils/util.py
@@ -117,6 +117,14 @@ class GPUMemoryMonitor:
 #     data = indepent_index_data(data, idx)
 #     return data, preds
 
+def get_q_error(est_card, card):
+    if card == 0 and est_card != 0:
+        return 1.0  # illegal query, will be dropped later, won't affect the result
+    if card != 0 and est_card == 0:
+        return card
+    if card == 0 and est_card == 0:
+        return 1.0
+    return max(est_card / card, card / est_card)
 
 def MakeOrdering(table, config, raw_config):
     fixed_ordering = None
@@ -470,28 +478,41 @@ def SampleTupleThenRandom(all_cols,
     key_idx = next((k for k, col in enumerate(all_cols) if col.name in ['id', 'movie_id']), -1)
 
     # 2. 过滤掉 key_idx，生成候选列的索引
-    remaining_cols_idx = [k for k in range(len(all_cols)) if k != key_idx]
+    remaining_cols_idx = np.array([k for k in range(len(all_cols)) if k != key_idx])
 
-    s = None
-    escape = 0
-    while s is None or len(remaining_cols_idx)-np.sum(pd.isnull(s))<num_filters:
-        s = table.data.iloc[rng.randint(0, table.cardinality)]
-        escape+=1
-        if escape>10:
-            num_filters-=1
-            escape = 0
-    vals = s.values
+    selected_cols = rng.binomial(1, 0.5, size=len(remaining_cols_idx)).astype(bool)
+    idxs = remaining_cols_idx[selected_cols]
+    # filter out columns with no distinct values except nan value
+    idxs = list(filter(lambda x: not ((all_cols[x].has_none and len(all_cols[x].all_distinct_values) <= 1) or (len(all_cols[x].all_distinct_values)==0)), idxs))
+    vals = []
+    for idx in idxs:
+        col = all_cols[idx]
+        dvs = col.all_distinct_values
+        start_idx = 1 if col.has_none else 0
+        v = dvs[rng.randint(start_idx, len(dvs))]
+        vals.append(np.array(v).astype(table.data.iloc[idx].dtype).item())
+    cols = np.take(all_cols, idxs)
+    # s = None
+    # escape = 0
+    # while s is None or len(remaining_cols_idx)-np.sum(pd.isnull(s))<num_filters:
+    #     s = table.data.iloc[rng.randint(0, table.cardinality)]
+    #     escape+=1
+    #     if escape>10:
+    #         num_filters-=1
+    #         escape = 0
+    # vals = s.values
 
-    if 'dmv' in dataset:
-        # Giant hack for DMV.
-        vals[6] = vals[6].to_datetime64()
+    # if 'dmv' in dataset:
+    #     # Giant hack for DMV.
+    #     vals[6] = vals[6].to_datetime64()
 
-    idxs = None
-    while idxs is None or pd.isnull(vals[idxs]).any():
-        idxs = rng.choice(remaining_cols_idx, replace=False, size=num_filters)
-        cols = np.take(all_cols, idxs)
+    # idxs = None
+    # while idxs is None or pd.isnull(vals[idxs]).any():
+    #     idxs = rng.choice(remaining_cols_idx, replace=False, size=num_filters)
+    #     cols = np.take(all_cols, idxs)
 
-    vals = vals[idxs]
+    # vals = vals[idxs]
+    # num_filters = len(vals)
     num_filters = len(vals)
     # If dom size >= 10, okay to place a range filter.
     # Otherwise, low domain size columns should be queried with equality.
@@ -528,7 +549,7 @@ def GenerateQuery(all_cols, rng, table, dataset, return_col_idx=False, num_filte
                 num_filters = rng.randint(5, 14)
         else:
             # num_filters = rng.randint(1, min(max(2, int(len(table.columns) * 0.3)), 4)) if how=='=' else rng.randint(max(1, int(len(table.columns) * 0.3)), len(table.columns))
-            num_filters = rng.randint(max(0, int(len(table.columns) * 0.5)), len(table.columns))
+            num_filters = rng.randint(0, len(table.columns))
     if num_filters==0:
         return [[],[],[]]
     cols, ops, vals = SampleTupleThenRandom(all_cols,
@@ -671,8 +692,23 @@ def _get_table_dict(tables):
 def _get_join_dict(joins, table_dict, use_alias_keys):
     from ordered_set import OrderedSet
     join_dict = defaultdict(OrderedSet)
+    how = None
     for j in joins:
-        ops = j.split('=')
+        if '>=' in j:
+            ops = j.split('>=')
+            how = '>='
+        elif '<=' in j:
+            ops = j.split('<=')
+            how = '<='
+        elif '>' in j:
+            ops = j.split('>')
+            how = '>'
+        elif '<' in j:
+            ops = j.split('<')
+            how = '<'
+        else:
+            ops = j.split('=')
+            how = '='
         op1 = ops[0].split('.')
         op2 = ops[1].split('.')
         t1, k1 = op1[0], op1[1]
@@ -682,7 +718,7 @@ def _get_join_dict(joins, table_dict, use_alias_keys):
             t2 = table_dict[t2]
         join_dict[t1].add(k1)
         join_dict[t2].add(k2)
-    return join_dict
+    return join_dict, how
 
 
 def _try_parse_literal(s):
@@ -727,16 +763,18 @@ def JobToQuery(csv_file, use_alias_keys=True):
     queries = []
     with open(csv_file) as f:
         data_raw = list(list(rec) for rec in csv.reader(f, delimiter='#'))
+        hows = []
         for row in data_raw:
             reader = csv.reader(row)  # comma-separated
             table_dict = _get_table_dict(next(reader))
-            join_dict = _get_join_dict(next(reader), table_dict, use_alias_keys)
+            join_dict, how = _get_join_dict(next(reader), table_dict, use_alias_keys)
+            hows.append(how)
             predicate_dict = _get_predicate_dict(next(reader), table_dict)
             true_cardinality = int(next(reader)[0])
             queries.append((list(table_dict.values()), join_dict,
                             predicate_dict, true_cardinality))
 
-        return queries
+        return queries, hows
 
 
 def UnpackQueries(table_dict, queries):